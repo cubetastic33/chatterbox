0aa89bfab2b59639c7b8cbcdcde4b73a276fba18
cubetastic33 <aravkasi@gmail.com>
2017-12-18 17:47:50 +0530

    updated files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDOv_3sUXUrPqF3SrBPmXiREQ-Ulm7VVh4",
+  authDomain: "chatterbox-3fc39.firebaseapp.com",
+  databaseURL: "https://chatterbox-3fc39.firebaseio.com",
+  projectId: "chatterbox-3fc39",
+  storageBucket: "chatterbox-3fc39.appspot.com",
+  messagingSenderId: "180111850333"
+};
+firebase.initializeApp(config);
+
 $(document).ready(function() {
   hideloader();
 });
